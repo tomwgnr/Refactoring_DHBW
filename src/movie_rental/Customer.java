9f7471dd9416b57cc7aa95aed6b560def3120f02
tomwgnr <tom.wagner@fn.de>
2021-05-11 20:54:55 +0200

    renamed some Variables
    Took 21 minutes

--- src/movie_rental/Customer.java
+++ src/movie_rental/Customer.java
@@ -5,15 +5,19 @@ import java.util.*;
 class Customer {
     private String name;
     private Vector rentals = new Vector();
+
     public Customer (String newname){
         name = newname;
-    };
+    }
+
     public void addRental(Rental arg) {
         rentals.addElement(arg);
-    };
+    }
+
     public String getName (){
         return name;
-    };
+    }
+
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
@@ -41,24 +45,25 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
+    private double amountFor(Rental rental) {
         double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+        switch (rental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += rental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
                 break;
             default:
                 System.out.println("There was an Error");
+                break;
         }
         return thisAmount;
     }
